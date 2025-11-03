45228b53865ea6c584e625473d687d0773b011dd
wiizzl <git@houlliere.com>
2025-11-03 12:39:11 +0100

    feat: update public key for pier in secrets.nix; update binary files for password.age and tailscale.age

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  pier = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINBuhGpkusmMSzJZL/gfl+k0ytUUWBean705sk4l1UzY";
+  pier = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHyWE+mk3eu5C0XdV9oCOHULBcnQT4BVUdq4YBZLugnI";
 in
 {
   "password.age".publicKeys = [ pier ];
