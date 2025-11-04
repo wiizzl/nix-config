5b90c305c7d156973b3891fa23c8e7c50222ad30
wiizzl <git@houlliere.com>
2025-11-04 22:27:50 +0100

    feat: update Zed editor configuration and add settings file

--- modules/home/apps/editor/zed/default.nix
+++ modules/home/apps/editor/zed/default.nix
@@ -11,41 +11,25 @@ let
 in
 {
   config = mkIf apps.editor.zed.enable {
-    home-manager.users.${user.name} = {
-      stylix = mkIf desktop.addons.stylix.enable {
-        targets.zed.enable = false;
+    home-manager.users.${user.name} =
+      { config, ... }:
+      {
+        stylix = mkIf desktop.addons.stylix.enable {
+          targets.zed.enable = false;
+        };
+
+        xdg.configFile."zed" = {
+          source = config.lib.file.mkOutOfStoreSymlink "${user.homeDir}/nix-config/modules/home/apps/editor/zed/config/";
+          recursive = true;
+        };
+
+        programs.zed-editor = {
+          enable = true;
+
+          extraPackages = with pkgs; [
+            nixd
+          ];
+        };
       };
-
-      # xdg.configFile."zed" = {
-      #   source = config.lib.file.mkOutOfStoreSymlink "./config";
-      #   recursive = true;
-      # };
-
-      programs.zed-editor = {
-        enable = true;
-
-        extraPackages = with pkgs; [
-          nixd
-        ];
-
-        extensions = [
-          "html"
-          "astro"
-          "nix"
-          "toml"
-          "php"
-          "dockerfile"
-          "sql"
-          "vue"
-          "xml"
-          "java"
-          "prisma"
-          "csharp"
-          "biome"
-          "catppuccin"
-          "catppuccin-icons"
-        ];
-      };
-    };
   };
 }
