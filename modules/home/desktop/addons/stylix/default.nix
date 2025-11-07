c9553a70c25c238a29e53f6c8a74b56ee52508e9
wiizzl <git@houlliere.com>
2025-11-07 20:27:55 +0100

    fix: stylix config

--- modules/home/desktop/addons/stylix/default.nix
+++ modules/home/desktop/addons/stylix/default.nix
@@ -10,11 +10,9 @@ let
   inherit (config.my) desktop user;
 in
 {
-  imports = [ inputs.stylix.nixosModules.stylix ];
-
   config = mkIf desktop.addons.stylix.enable {
     home-manager.users.${user.name} = {
-      imports = [ inputs.stylix.homeModules.stylix ];
+      # imports = [ inputs.stylix.homeModules.stylix ];
     };
   };
 }
