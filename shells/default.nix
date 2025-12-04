647f6191f7f5cd75e406ccffbcfa761f8d24e021
wiizzl <git@houlliere.com>
2025-12-04 20:55:34 +0100

    feat: add flake.lock files for various shells and update default.nix with additional environments

--- shells/default.nix
+++ shells/default.nix
@@ -1,19 +1,46 @@
-{ lib, ... }:
-
-let
-  getShells =
-    dir:
-    builtins.map
-      (folder: {
-        path = ./. + "/${folder}";
-        description = "${folder} development environment";
-      })
-      (
-        builtins.filter (folder: lib.hasFile "flake.nix" (dir + "/${folder}")) (
-          builtins.attrNames (builtins.readDir dir)
-        )
-      );
-in
 {
-  imports = getShells ./.;
+  default = {
+    path = ./empty;
+    description = "Empty development environment";
+  };
+  bun = {
+    path = ./bun;
+    description = "Bun development environment";
+  };
+  c-cpp = {
+    path = ./c-cpp;
+    description = "C/C++ development environment";
+  };
+  go = {
+    path = ./go;
+    description = "Go (Golang) development environment";
+  };
+  java = {
+    path = ./java;
+    description = "Java development environment";
+  };
+  nix = {
+    path = ./nix;
+    description = "Nix development environment";
+  };
+  node = {
+    path = ./node;
+    description = "Node.js development environment";
+  };
+  php = {
+    path = ./php;
+    description = "PHP development environment";
+  };
+  python = {
+    path = ./python;
+    description = "Python development environment";
+  };
+  rust = {
+    path = ./rust;
+    description = "Rust development environment";
+  };
+  zig = {
+    path = ./zig;
+    description = "Zig development environment";
+  };
 }
