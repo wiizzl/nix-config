2e43bd1a360412d786778eb31ee000eefce26a1f
wiizzl <git@houlliere.com>
2025-12-19 11:38:22 +0100

    feat: update flake.lock and vscode configuration for dependency versions and new settings

--- modules/home/apps/editor/vscode/default.nix
+++ modules/home/apps/editor/vscode/default.nix
@@ -24,58 +24,58 @@ in
 
         programs.vscode = {
           enable = true;
+          mutableExtensionsDir = false;
 
           profiles.default = {
-            extensions = with pkgs.nix-vscode-extensions.vscode-marketplace-release; [
-              dsznajder.es7-react-js-snippets
-              wayou.vscode-todo-highlight
-              astro-build.astro-vscode
-              jnoortheen.nix-ide
-              adpyke.codesnap
-              naumovs.color-highlight
-              catppuccin.catppuccin-vsc
-              thang-nm.flow-icons
-              usernamehw.errorlens
-              yoavbls.pretty-ts-errors
-              dbaeumer.vscode-eslint
-              # biomejs.biome
-              prisma.prisma
-              golang.go
-              ziglang.vscode-zig
-              rust-lang.rust-analyzer
-              vue.volar
-              ms-vsliveshare.vsliveshare
-              unifiedjs.vscode-mdx
-              quicktype.quicktype
-              esbenp.prettier-vscode
-              bradlc.vscode-tailwindcss
-              ms-python.python
-              ms-python.debugpy
-              ms-python.vscode-python-envs
-              charliermarsh.ruff
-              meganrogge.template-string-converter
-              github.copilot-chat
-              github.vscode-github-actions
-              github.vscode-pull-request-github
-              icrawl.discord-vscode
-              tamasfe.even-better-toml
-              redhat.vscode-yaml
-              redhat.java
-              bmewburn.vscode-intelephense-client
-              junstyle.php-cs-fixer
-              ms-vscode.remote-explorer
-              ms-vscode-remote.remote-containers
-              ms-vscode-remote.remote-ssh
-              ms-vscode-remote.remote-ssh-edit
-              ms-azuretools.vscode-containers
-              formulahendry.auto-close-tag
-              formulahendry.auto-rename-tag
-              oven.bun-vscode
-              # ms-dotnettools.csdevkit
-              # ms-dotnettools.vscode-dotnet-runtime
-              # ms-dotnettools.dotnet-maui
-              # ms-dotnettools.csharp
-            ];
+            extensions =
+              with pkgs.open-vsx;
+              [
+                dsznajder.es7-react-js-snippets
+                wayou.vscode-todo-highlight
+                astro-build.astro-vscode
+                jnoortheen.nix-ide
+                adpyke.codesnap
+                naumovs.color-highlight
+                catppuccin.catppuccin-vsc
+                thang-nm.flow-icons
+                usernamehw.errorlens
+                yoavbls.pretty-ts-errors
+                dbaeumer.vscode-eslint
+                prisma.prisma
+                ziglang.vscode-zig
+                rust-lang.rust-analyzer
+                vue.volar
+                unifiedjs.vscode-mdx
+                esbenp.prettier-vscode
+                bradlc.vscode-tailwindcss
+                ms-python.python
+                ms-python.debugpy
+                ms-python.vscode-python-envs
+                charliermarsh.ruff
+                meganrogge.template-string-converter
+                github.vscode-github-actions
+                icrawl.discord-vscode
+                tamasfe.even-better-toml
+                redhat.vscode-yaml
+                redhat.java
+                bmewburn.vscode-intelephense-client
+                junstyle.php-cs-fixer
+                formulahendry.auto-close-tag
+                formulahendry.auto-rename-tag
+                oven.bun-vscode
+                # biomejs.biome
+              ]
+              ++ (with pkgs.nix-vscode-extensions.vscode-marketplace-release; [
+                golang.go
+                github.copilot-chat
+                ms-vsliveshare.vsliveshare
+                quicktype.quicktype
+                ms-vscode.remote-explorer
+                ms-vscode-remote.remote-containers
+                ms-vscode-remote.remote-ssh
+                ms-vscode-remote.remote-ssh-edit
+                ms-azuretools.vscode-containers
+              ]);
           };
         };
       }
